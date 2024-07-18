30e1dd4109054016eb0d9bd28b45b5e5b1d67447
Utsav Chaudhary <utsavchaudhary.ws@gmail.com>
2024-07-18 01:36:24 -0400

    Project files setup

---
+++ Assets/Scripts/SerialHandler.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using SerialPortUtility;
+
+[RequireComponent(typeof(SerialPortUtilityPro))]
+public class SerialHandler : MonoBehaviour
+{
+    private SerialPortUtilityPro serialPort;
+    private string previousSentData;
+
+    private void Start()
+    {
+        serialPort = GetComponent<SerialPortUtilityPro>();
+
+        if (!serialPort)
+        {
+            serialPort = FindObjectOfType<SerialPortUtilityPro>();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (serialPort && serialPort.IsOpened())
+        {
+            serialPort.Close();
+        }
+    }
+
+    private void SendSerialData(string serialMessage)
+    {
+        if (!serialPort.IsConnected())
+        {
+            Debug.Log("Serial port not connected!");
+            return;
+        }
+
+        if (!serialPort.IsOpened())
+        {
+            Debug.LogError("Serial port not opened!");
+            return;
+        }
+
+        if (serialMessage == previousSentData)
+        {
+            return;
+        }
+
+        try
+        {
+            if (serialPort.WriteLF(serialMessage))
+            {
+                previousSentData = serialMessage;
+                Debug.Log(previousSentData);
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+    }
+
+    private void Update()
+    {
+        //Debug.Log(ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.Elbow));
+
+        SendSerialData(ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.Finger).ToString() + "," +
+            ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.Wrist).ToString() + "," +
+            ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.Palm).ToString() + "," +
+            ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.Elbow).ToString() + "," +
+            ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.ShoulderLateral).ToString() + "," +
+            ServoMapper.Instance.GetAngle(ServoMapper.Side.R, ServoMapper.BodyJoint.ShoulderForward).ToString());
+    }
+}
---
+++ Assets/Scripts/ServoMapper.cs
@@ -0,0 +1,118 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class ServoMapper : MonoBehaviour
+{
+    public enum Side { L, R }
+    public enum BodyJoint { ShoulderForward, ShoulderLateral, Elbow, Palm, Wrist, Finger }
+
+    [System.Serializable]
+    private class Arm
+    {
+        [SerializeField] private Transform shoulder;
+        [SerializeField] private Transform elbow;
+        [SerializeField] private Transform wrist;
+        [SerializeField] private Transform finger;
+
+        private Transform avatar;
+        private InputDevice inputDevice;
+
+        public void Initialize(Transform _avatar, Side side)
+        {
+            avatar = _avatar;
+
+            List<InputDevice> devices = new();
+
+            if (side == Side.L)
+            {
+                InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Left | InputDeviceCharacteristics.Controller, devices);
+            }
+            else
+            {
+                InputDevices.GetDevicesWithCharacteristics(InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller, devices);
+            }
+
+            if (devices.Count > 0)
+            {
+                inputDevice = devices[0];
+            }
+        }
+
+        public int GetAngle(BodyJoint joint)
+        {
+            switch (joint)
+            {
+                case BodyJoint.ShoulderForward:
+                    if (shoulder && avatar)
+                    {
+                        Vector3 shoulderDirection = Vector3.ProjectOnPlane(-shoulder.right, avatar.right);
+                        return Mathf.RoundToInt(180f - Vector3.Angle(shoulderDirection, avatar.forward));
+                    }
+                    break;
+                case BodyJoint.ShoulderLateral:
+                    if (shoulder && avatar)
+                    {
+                        Vector3 shoulderDirection = Vector3.ProjectOnPlane(-shoulder.right, avatar.forward);
+                        return Mathf.RoundToInt(Vector3.Angle(shoulderDirection, avatar.up));
+                    }
+                    break;
+                case BodyJoint.Elbow:
+                    if (shoulder && elbow && wrist)
+                    {
+                        return Mathf.RoundToInt(Vector3.Angle(elbow.position - shoulder.position, wrist.position - elbow.position));
+                    }
+                    break;
+                case BodyJoint.Palm:
+                    if (elbow && wrist && finger && avatar)
+                    {
+                        int angle = Mathf.RoundToInt(Vector3.Angle(wrist.position - elbow.position, finger.position - wrist.position));
+                        Vector3 palmDirection = Vector3.ProjectOnPlane(-wrist.right, avatar.forward);
+                        return 90 + Mathf.RoundToInt(Vector3.Angle(palmDirection, avatar.right) < 90f ? angle : -angle);
+                    }
+                    break;
+                case BodyJoint.Wrist:
+                    if (wrist && avatar)
+                    {
+                        Vector3 wristDirection = Vector3.ProjectOnPlane(wrist.up, avatar.forward);
+                        return Mathf.RoundToInt(180f - Vector3.Angle(wristDirection, avatar.up));
+                    }
+                    break;
+                case BodyJoint.Finger:
+                    if (inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+                    {
+                        return Mathf.RoundToInt(triggerValue * 180f);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            return 90;
+        }
+    }
+
+    public static ServoMapper Instance { get; private set; }
+
+    [Header("Transform References")]
+
+    [SerializeField] private Transform avatar;
+    [SerializeField] private Arm leftArm;
+    [SerializeField] private Arm rightArm;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        leftArm.Initialize(avatar, Side.L);
+        rightArm.Initialize(avatar, Side.R);
+    }
+
+    public int GetAngle(Side side, BodyJoint joint)
+    {
+        return side == Side.L ? leftArm.GetAngle(joint) : rightArm.GetAngle(joint);
+    }
+}
