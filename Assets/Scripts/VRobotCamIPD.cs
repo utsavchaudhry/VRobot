42cc864be3cd1657f6a4acfac90d40310914fcf9
Utsav Chaudhary <utsavchaudhary.ws@gmail.com>
2024-11-21 15:52:56 -0500

    ipd

--- Assets/Scripts/VRobotCamIPD.cs
+++ Assets/Scripts/VRobotCamIPD.cs
@@ -6,11 +6,13 @@ public class VRobotCamIPD : MonoBehaviour
     [SerializeField] private Transform right;
     [SerializeField] [Range(0f, 250f)] private float range = 200f;
     [SerializeField] private float sensitivity = 10f;
+    [SerializeField] private bool testWithKeyboard;
 
     private Vector3 originalPositionLeft;
     private Vector3 originalPositionRight;
     private float ipdOffset;
     private string ipdSaveKey = "IPD";
+    private bool setup;
 
     private Vector3 GetPosition(Transform t)
     {
@@ -27,6 +29,12 @@ public class VRobotCamIPD : MonoBehaviour
         originalPositionLeft = GetPosition(left);
         originalPositionRight = GetPosition(right);
         ipdOffset = PlayerPrefs.GetFloat(ipdSaveKey);
+
+#if !UNITY_EDITOR
+
+        testWithKeyboard = false;
+
+#endif
     }
 
     private void SetPosition(Transform t, Vector3 position)
@@ -46,13 +54,14 @@ public class VRobotCamIPD : MonoBehaviour
             return;
         }
 
-        float ipdDelta = InputManager.LeftController.Joystick.x * sensitivity * Time.deltaTime;
+        float ipdDelta = (testWithKeyboard ? Input.GetAxis("Horizontal") : InputManager.LeftController.Joystick.x) * sensitivity * Time.deltaTime;
 
-        if (ipdDelta != 0f)
+        if (ipdDelta != 0f || !setup)
         {
             ipdOffset = Mathf.Clamp(ipdOffset + ipdDelta, -range, range);
             SetPosition(left, (Vector3.right * ipdOffset) + originalPositionLeft);
             SetPosition(right, (Vector3.left * ipdOffset) + originalPositionRight);
+            setup = true;
             PlayerPrefs.SetFloat(ipdSaveKey, ipdOffset);
         }
     }
